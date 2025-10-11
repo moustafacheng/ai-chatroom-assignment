03ce52b8677fcdb332d552854d628921ca81a7bc
moustafacheng <moustafacheng@gmail.com>
2025-10-12 01:44:23 +0800

    remove unnecessary files

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,21 +1,19 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    redirect: '/home',
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    redirect: "/home",
     children: [
-      { path: 'home', component: () => import('pages/IndexPage.vue') },
-      { path: 'colors', component: () => import('pages/ColorsPage.vue') },
-      { path: 'typography', component: () => import('pages/TypographyPage.vue') },
-    ]
+      { path: "home", component: () => import("pages/IndexPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
